1f49a9559fb35b31d999d033a59c640b13fa7222
karthi0811 <karthimeyyappan.cse@gmail.com>
2022-06-13 11:32:06 +0530

    hello java

---
+++ src/org/test/SumValues.java
@@ -0,0 +1,18 @@
+package org.test;
+
+public class SumValues {
+	
+	public static void main(String[] args) {
+		
+		int sum = 0;
+		int []a= {1,2,3,4,5,6,7,8,9,10};
+		for (int i=0;i<10;i++) {
+			sum = sum + a[i];
+			
+		}
+	
+		System.out.println(sum);
+	}
+	
+
+}
