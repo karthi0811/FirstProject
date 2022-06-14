1ceea9a85aa658910270139fa3aa8aee6f6edd95
karthi0811 <karthimeyyappan.cse@gmail.com>
2022-06-14 09:58:22 +0530

    code update

--- src/org/test/SumValues.java
+++ src/org/test/SumValues.java
@@ -5,13 +5,17 @@ public class SumValues {
 	public static void main(String[] args) {
 		
 		int sum = 0;
+		int sum1 =0;
 		int []a= {1,2,3,4,5,6,7,8,9,10};
 		for (int i=0;i<10;i++) {
 			sum = sum + a[i];
+			sum1 = sum1 + a[i];
 			
 		}
 	
 		System.out.println(sum);
+		System.out.println(sum1);
+		
 	}
 	
 
